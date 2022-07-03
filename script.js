c6adf9d771c8268fddb68f971de353d409bd6d3d
John Monk <johntmonk1@gmail.com>
2022-07-03 16:21:13 -0500

    made css responsive, updated styling, improved js

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ const submit = document.querySelector('.contents__btn');
 const contents = document.querySelector('.contents');
 const thankyou = document.querySelector('.submission');
 const scoreFeedback = document.querySelector('.submission__outof');
-
-submit.addEventListener('click', () => {
-    contents.classList.add('hide');
-    thankyou.classList.remove('hide');
-})
+let score;
 
 for(let button of buttons){
     button.addEventListener('click', () => {
         buttons.forEach( bt => bt.classList.toggle('active__color',bt===button))
 
-        let score = button.textContent;
+        score = button.textContent;
 
         scoreFeedback.textContent = `You selected ${score} out of 5`;
     })
 }
+
+submit.addEventListener('click', () => {
+    if (!score) {
+        alert('Please select a rating.')
+    } else {
+        contents.classList.add('hide');
+        thankyou.classList.remove('hide');
+    }
+})
